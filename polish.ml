06b088286327d7bd2efbeb41d4423fccf798c0be
Yoan Rougeolle <yoyo031201@orange.fr>
2021-11-23 21:50:09 +0100

    Changement de la fonction read_file qui associe les lignes maintenant, et optimisation de la fonction is_string_from_alpha

--- polish.ml
+++ polish.ml
@@ -49,37 +49,26 @@ type program = block
 
 (***********************************************************************)
 
-let read_file (filename:string) : string list =
+let read_file (filename:string) : (string * int) list =
   let ci = open_in filename in
-  let rec read ci = 
+  let rec read ci num = 
     try 
       let line = input_line ci in
-      line :: (read ci)
+      (line, num) :: (read ci (num + 1))
     with End_of_file -> []
-  in read ci;;
+  in read ci 1;;
 
 
-let explode str =
-  let rec loop index acc =
-    if index < 0 then acc
-    else loop (index - 1) (str.[index] :: acc)
-  in
-  loop (String.length str - 1) []
-;;
-
-let rec is_in_list elt list = 
-  match list with
-  | [] -> false
-  | x :: tail -> (x = elt) || (is_in_list elt tail)
-
-let is_string_from_alpha (mot_string:string) (alpha_string:string) =
-  let rec is_string_from_alpha_list (mot:char list) (alpha:char list): bool = 
-    match mot with
-    | [] -> true
-    | x :: tail -> (is_in_list x alpha) && (is_string_from_alpha_list tail alpha)
-  in let mot = explode mot_string and alpha = explode alpha_string
-  in is_string_from_alpha_list mot alpha
-;;
+let is_string_from_alpha (mot:string) (alpha:string) =
+  let size_alpha = String.length alpha
+  and size_mot = String.length mot in
+  let rec is_string_from_alpha_loop i =
+    let rec is_in_string c j =
+      if j >= size_alpha then false
+      else ((alpha.[j] = c) || (is_in_string c (j+1)))
+    in if i >= size_mot then true
+    else ((is_in_string mot.[i] 0) && (is_string_from_alpha_loop (i+1)))
+  in is_string_from_alpha_loop 0;;
 
 let is_number (mot:string) = is_string_from_alpha mot "0123456789"
 let is_operator (mot:string) = is_string_from_alpha mot "+-*/%"
@@ -178,6 +167,13 @@ let rec print_list_string l =
   | [] -> print_newline()
   | x :: ll -> print_string x; print_newline();print_list_string ll;;
 
+let rec print_list_int_string l =
+  match l with
+  | [] -> print_newline()
+  | (s, x) :: ll -> print_int x;print_string(" "); 
+                    print_string s;print_newline(); 
+                    print_list_int_string ll;;
+
 let rec print_list_char lc =
   match lc with 
   | [] -> print_newline()
@@ -203,9 +199,7 @@ let print_expr e =
 
 (** TEST *)
 let l = read_file "./exemples/abs.p";;
-print_list_string l;;
-
-let lc = explode "salut";;
+print_list_int_string l;;
 
 print_bool(is_string_from_alpha "158648613" "0123456789");;
 
