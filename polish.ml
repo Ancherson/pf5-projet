a238b7edf3ed7813c316331f5882178ec31a7543
ROUGEOLLE yoan <yoyo031201@orange.fr>
2021-11-28 19:28:22 +0100

    Resolve "Ajouter les bons messages d'erreurs lors du read"

--- polish.ml
+++ polish.ml
@@ -4,6 +4,8 @@
     polish.ml. Il est recommandé d'architecturer ultérieurement votre
     projet en plusieurs fichiers source de tailles raisonnables *)
 
+open My_exception;;
+
 (*****************************************************************************)
 (** Syntaxe abstraite Polish (types imposés, ne pas changer sauf extensions) *)
 
@@ -79,22 +81,23 @@ let is_variable (mot:string) =
   List.for_all (fun elt -> (mot <> elt)) ["+"; "-"; "*"; "/"; "%"; "<"; ">"; "<="; ">="; "="; "<>"; ":="]
   && (not (is_number mot));;
 
-let rec read_expression (l:string list) : expr * string list =
+let rec read_expression (l:string list) (num_line: int) : expr * string list =
   match l with
-  | [] -> failwith "pbm" 
+  | [] -> raise(Error_Read (num_line, "It's not an expression !"))
   | x :: ll -> 
     if is_number x then (Num(int_of_string x), ll)
     else if is_operator x then
-      let (e1, lll) = read_expression ll in
-      let (e2, end_list) = read_expression lll in
+      let (e1, lll) = read_expression ll num_line in
+      let (e2, end_list) = read_expression lll num_line in
       match x with
       | "+" -> (Op(Add, e1, e2), end_list)
       | "-" -> (Op(Sub, e1, e2), end_list)
       | "*" -> (Op(Mul, e1, e2), end_list)
       | "/" -> (Op(Div, e1, e2), end_list)
       | "%" -> (Op(Mod, e1, e2), end_list)
-      | _ -> failwith "pbm operator"
-    else if (List.exists (fun elt -> (elt = x)) ["<"; ">"; "<="; ">="; "="; "<>"; ":="]) then failwith "not a variable"
+      | _ -> raise(Error_Read (num_line, "Not an expression !"))
+    else if (List.exists (fun elt -> (elt = x)) ["<"; ">"; "<="; ">="; "="; "<>"; ":="]) 
+      then raise(Error_Read (num_line, "Not an expression, wait a variable !"))
     else (Var(x), ll)
 ;;
 
@@ -120,56 +123,56 @@ let get_line_elem_and_Nspace (line : string) : int * string list =
   (count_first_space line ,suppr_empty_string_list (String.split_on_char ' ' line ));;
 
 
-
-
-
-let aux_read_condition (mots :string list) (first_expr :expr) (comparator : comp) : cond =
-  let (last_expr,tail2) = read_expression mots in
+let aux_read_condition (mots :string list) (first_expr :expr) (comparator : comp) (num_line: int) : cond =
+  let (last_expr,tail2) = read_expression mots num_line in
   match tail2 with
   |[] -> (first_expr,comparator,last_expr)
-  |_ -> failwith "not only a condition"
+  |_ -> raise(Error_Read (num_line, "It's not only a condition"))
 ;;
 
-let read_condition (mots :string list)  : cond=
-  let (first_expr,tail1) = read_expression mots in
+let read_condition (mots :string list) (num_line: int) : cond=
+  let (first_expr,tail1) = read_expression mots num_line in
   match tail1 with 
-  |[] -> failwith "No condition"
+  |[] -> raise(Error_Read (num_line, "It's just an expression, not a condition !"))
   |x::ll -> match x with
-    |"=" ->(*Equals *) aux_read_condition ll first_expr Eq
-    |"<" ->(*Inf *) aux_read_condition ll first_expr Lt
-    |">" ->(*Sup *) aux_read_condition ll first_expr Gt
-    |"<>" ->(*Diff *) aux_read_condition ll first_expr Ne
-    |"<=" ->(*Equals or inf *) aux_read_condition ll first_expr Le
-    |">=" ->(*Equals or sup *) aux_read_condition ll first_expr Ge
-    | _ -> failwith "Not a condition"
+    |"=" ->(*Equals *) aux_read_condition ll first_expr Eq num_line
+    |"<" ->(*Inf *) aux_read_condition ll first_expr Lt num_line
+    |">" ->(*Sup *) aux_read_condition ll first_expr Gt num_line
+    |"<>" ->(*Diff *) aux_read_condition ll first_expr Ne num_line
+    |"<=" ->(*Equals or inf *) aux_read_condition ll first_expr Le num_line
+    |">=" ->(*Equals or sup *) aux_read_condition ll first_expr Ge num_line
+    | _ -> raise(Error_Read (num_line, "It's not a condition, wait a comparator !"))
 ;;
 
-let read_print (mots: string list) : instr = 
+let read_print (mots: string list) (num_line: int) : instr = 
   match mots with
-  | [] -> failwith "too few argument print"
-  | l -> let (exp, rest) = read_expression l in
-         if rest <> [] then failwith "not an expression print"
-         else Print(exp)
+  | [] -> raise(Error_Read (num_line, "Need argument after Print"))
+  | l -> 
+    let (exp, rest) = read_expression l num_line in
+    if rest <> [] then raise(Error_Read (num_line, "Too much argument after PRINT"))
+    else Print(exp)
              
 
-let read_read (mots: string list) : instr = 
+let read_read (mots: string list) (num_line: int) : instr = 
   match mots with
-  | [] -> failwith "too few argument read"
-  | x :: empty_list -> if empty_list <> [] then failwith "too much arguments read"
-               else if is_variable x then Read(x)
-               else failwith "not a variable read"
+  | [] -> raise(Error_Read (num_line, "Need a variable after READ"))
+  | x :: empty_list -> 
+    if empty_list <> [] 
+      then raise(Error_Read (num_line, "Too much argument after READ"))
+    else if is_variable x then Read(x)
+    else raise(Error_Read (num_line, "Wait a variable name after READ"))
 
-let read_set(mots: string list) : instr = 
+let read_set(mots: string list) (num_line: int) : instr = 
   match mots with
-  | [] -> failwith "not a set instruction 1"
-  | x1 :: ll -> if not (is_variable x1) then failwith "not a set instruction 2"
+  | [] -> raise(Error_Read (num_line, "Erreur set"))
+  | x1 :: ll -> if not (is_variable x1) then raise(Error_Read (num_line, "Not a variable name in the SET instruction !"))
     else
       match ll with
-      | [] -> failwith "not a set instruction 3"
-      | x2 :: lll -> if x2 <> ":=" then failwith "not a set instruction 4"
+      | [] -> raise(Error_Read (num_line, "Not just a variable in a SET instruction !"))
+      | x2 :: lll -> if x2 <> ":=" then raise(Error_Read (num_line, "Need := after the variable in a SET instruction !"))
         else 
-          let (ex, empty_list) = read_expression lll in
-          if empty_list <> [] then failwith "not a set instruction 5"
+          let (ex, empty_list) = read_expression lll num_line in
+          if empty_list <> [] then raise(Error_Read (num_line, "Need an expression after := !"))
           else Set(x1, ex)
 
 let rec read_block (pro : int) (lines : (string * int) list) (is_else: bool) :block * ((string * int) list) =
@@ -180,11 +183,11 @@ let rec read_block (pro : int) (lines : (string * int) list) (is_else: bool) :bl
     if space_num < pro 
       then if (space_num mod 2) = 0 
         then ([],lines)
-      else failwith "Une ligne n'a pas de profondeur paire"
+      else raise(Error_Read (pos, "Error indentation not an even !"))
     else if space_num > pro 
-      then failwith "Probleme indentation"
+      then raise(Error_Read (pos, "Error indentation too deep !"))
     else if is_else
-      then read_else pro line lines 
+      then read_else pro line lines pos
     else let (instruction, next_block_lines)= read_instr space_num line pos next_lines in
       let (block1,next_next) = read_block space_num next_block_lines false in
       if instruction = None 
@@ -193,34 +196,32 @@ let rec read_block (pro : int) (lines : (string * int) list) (is_else: bool) :bl
 
 and read_instr (pro:int) (line: string list) (position: int) (next_lines: (string*int) list) : 'instr option * (string * int) list=
   match line with 
-  |[] -> failwith "Pb instruction"
+  |[] -> raise(Error_Read (position, "Erreur empty line !"))
   |x::tail -> match x with 
     |"COMMENT" -> (None,next_lines)
-    |"READ" -> (Some (read_read tail),next_lines)
-    |"PRINT" -> (Some (read_print tail),next_lines)
-    |"ELSE" -> failwith "manque un IF"
-    |"WHILE" -> let (while_instr,line_after_Wblock) = read_while pro tail next_lines in
+    |"READ" -> (Some (read_read tail position),next_lines)
+    |"PRINT" -> (Some (read_print tail position),next_lines)
+    |"ELSE" -> raise(Error_Read (position, "ELSE without IF precendently !"))
+    |"WHILE" -> let (while_instr,line_after_Wblock) = read_while pro tail next_lines position in
       (Some(while_instr),line_after_Wblock)
-    |"IF" -> let (if_instr, line_after_Iblock) = read_if pro tail next_lines in
+    |"IF" -> let (if_instr, line_after_Iblock) = read_if pro tail next_lines position in
       (Some(if_instr), line_after_Iblock)
-    |_ -> try 
-      (Some (read_set line),next_lines)
-      with Failure (s)-> print_string x; failwith "coucou"
-and read_while (pro: int) (mots: string list) (lignes: (string * int) list) : instr * ((string * int) list)=
-  let condition = read_condition mots in
+    |_ -> (Some (read_set line position),next_lines)
+and read_while (pro: int) (mots: string list) (lignes: (string * int) list) (num_line: int) : instr * ((string * int) list)=
+  let condition = read_condition mots num_line in
   let block1,next = read_block (pro + 2) lignes false in
   (While(condition, block1), next)
 
-and read_else (pro: int) (mots: string list) (lignes: (string * int) list) : block * ((string * int) list) = 
+and read_else (pro: int) (mots: string list) (lignes: (string * int) list) (num_line: int) : block * ((string * int) list) = 
   match mots with
-  | [] -> failwith "not else 1"
+  | [] -> raise(Error_Read (num_line, "Erreur ELSE"))
   | x :: tail -> 
     if(x = "ELSE" && tail = []) 
       then read_block (pro + 2) (List.tl lignes) false
     else read_block pro lignes false
 
-and read_if (pro: int) (mots: string list) (lignes: (string * int) list) : instr * ((string * int) list)=
-  let condition = read_condition mots in
+and read_if (pro: int) (mots: string list) (lignes: (string * int) list) (num_line: int) : instr * ((string * int) list)=
+  let condition = read_condition mots num_line in
   let block1, next = read_block (pro + 2) lignes false in
   let block2, next_next = read_block pro next true in
   (If(condition, block1, block2), next_next)
@@ -297,6 +298,7 @@ print_list_int_string_super l;;
 print_bool(is_number "-1");;
 print_bool(is_variable "14a");;
 
-let (e,ll) = read_expression ["/"; "truc"; "*"; "+"; "bla";"2";"3"];;
+let (e,ll) = read_expression ["/"; "truc"; "*"; "+"; "bla";"2";"3"] 0;;
 print_expr e;;
-read_block 0 l false;;
+(*read_block 0 l false;;*)
+check_error_read (fun () -> read_block 0 l false);;
