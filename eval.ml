0bf3be274a9df76c0bbda3d8aeb46c0e8bdb2f0c
cherion <billy@BillyTheKid.localdomain>
2021-12-10 08:54:57 +0100

    des exceptions plus jolies et plus logique avec des failwith à certain endroits

--- eval.ml
+++ eval.ml
@@ -5,22 +5,23 @@ open My_exception;;
 
 module Env = Map.Make (String) ;;
 
-let eval_op (o:op) :int -> int -> int =
+let eval_op (o:op) (line : int):int -> int -> int =
     match o with
     |Add -> (fun n1 n2 -> n1 + n2 )
     |Sub -> (fun n1 n2 ->  n1 - n2)
     |Mul -> (fun n1 n2 -> n1 * n2 )
-    |Div -> (fun n1 n2 -> if n2 = 0 then raise (Error_Eval "division by zero")
+    |Div -> (fun n1 n2 -> if n2 = 0 then raise (Error_Eval (line,"division by zero"))
         else n1 / n2)
-    |Mod -> (fun n1 n2 -> if n2 = 0 then raise (Error_Eval "division by zero")
+    |Mod -> (fun n1 n2 -> if n2 = 0 then raise (Error_Eval (line,"division by zero"))
         else n1 mod (n2))
 ;;
 
-let rec eval_expr (ex : expr) (env: int Env.t) :int =
+let rec eval_expr (ex : expr) (env: int Env.t) (line : int) :int =
     match ex with
     |Num x -> x
-    |Var x -> Env.find x env
-    |Op (o,exp1,exp2) -> eval_op o (eval_expr exp1 env) (eval_expr exp2 env)
+    |Op (o,exp1,exp2) -> (eval_op o line) (eval_expr exp1 env line) (eval_expr exp2 env line)
+    |Var x -> try Env.find x env
+        with Not_found -> raise (Error_Eval (line,"variable "^x^" not initialized"))
 ;;
 
 let eval_comp (com : comp) : int -> int -> bool =
@@ -32,29 +33,29 @@ let eval_comp (com : comp) : int -> int -> bool =
     | Gt -> (fun x y -> x > y)
     | Ge -> (fun x y -> x >= y)
 
-let eval_cond (co : cond) (env: int Env.t) : bool =
+let eval_cond (co : cond) (env: int Env.t) (line : int) : bool =
     match co with
-    | (e1, com, e2) -> (eval_comp com) (eval_expr e1 env) (eval_expr e2 env)
+    | (e1, com, e2) -> (eval_comp com) (eval_expr e1 env line) (eval_expr e2 env line)
 
 (*à chaque appel de eval_set faire :
 let env = eval_set xxxx env;; *)
-let eval_set (set:instr) (env: int Env.t) : int Env.t=
+let eval_set (set:instr) (env: int Env.t) (line : int): int Env.t=
     match set with
-    |Set(name,exp) -> let num = eval_expr exp env in
+    |Set(name,exp) -> let num = eval_expr exp env line in
         if not(Env.mem name env) then let env = Env.add name num env in
            env
         else
            let env = Env.remove name env in
            let env = Env.add name num env in
            env
-    |_ -> raise (Error_Eval "Should be a Set")
+    |_ -> failwith "Error eval Set"
 ;;
 
-let eval_print (print:instr) (env: int Env.t) :unit=
+let eval_print (print:instr) (env: int Env.t) (line : int) :unit=
     match print with
-    |Print(exp) -> print_int (eval_expr exp env);
+    |Print(exp) -> print_int (eval_expr exp env line);
         print_string "\n"
-    |_ -> raise (Error_Eval "Should be a Print")
+    |_ -> failwith "Error eval Print"
 ;;
 
 let eval_read (read : instr) (env: int Env.t) : int Env.t =
@@ -68,40 +69,40 @@ let eval_read (read : instr) (env: int Env.t) : int Env.t =
            let env = Env.remove s env in
            let env = Env.add s num env in
            env
-    | _ -> raise (Error_Eval "Should be a read")
+    | _ -> failwith "Error eval Read"
 ;;
 
 let rec eval_block (bloc : block) (env : int Env.t) : int Env.t = 
     match bloc with
     | [] -> env
     | (num,inst)::tail ->
-        let env = (eval_instr inst env) in
+        let env = (eval_instr inst env num) in
         eval_block tail env
 
-and eval_instr (ins : instr) (env : int Env.t) : int Env.t =
+and eval_instr (ins : instr) (env : int Env.t) (line : int): int Env.t =
     match ins with
-    | Set(_,_) -> eval_set ins env
+    | Set(_,_) -> eval_set ins env line
     | Read(_) -> eval_read ins env
-    | Print(_) -> (eval_print ins env); env
-    | If(_,_,_) -> eval_if ins env
-    | While(_,_) -> eval_while ins env
+    | Print(_) -> (eval_print ins env line); env
+    | If(_,_,_) -> eval_if ins env line
+    | While(_,_) -> eval_while ins env line
 
-and eval_if (ins : instr) (env : int Env.t) : int Env.t =
+and eval_if (ins : instr) (env : int Env.t) (line : int): int Env.t =
     match ins with
     | If(con, b1, b2) ->
-        if eval_cond con env then eval_block b1 env
+        if eval_cond con env line then eval_block b1 env
         else eval_block b2 env
-    | _ -> raise (Error_Eval "Should be  a if")
+    | _ -> failwith "Error eval If"
 
-and eval_while (ins : instr) (env : int Env.t) = 
+and eval_while (ins : instr) (env : int Env.t) (line : int) = 
     match ins with
     | While(con, b) -> 
         let rec while_loop co bloc env  =
-            if eval_cond co env
+            if eval_cond co env line
                 then while_loop co bloc (eval_block bloc env)
                 else env
         in while_loop con b env
-    | _ -> raise (Error_Eval "Should be a while")
+    | _ -> failwith "Error eval While"
         
 
 let eval_prog (p : program) : unit =
--- my_exception.ml
+++ my_exception.ml
@@ -1,14 +1,26 @@
 (** Toutes les exceptions + leurs fonctions associées *)
 
 exception Error_Read of (int * string)
-exception Error_Eval of (string)
+exception Error_Eval of (int * string)
 
 let get_string_error_read (e: (int * string)) : string =
     match e with
     | (n, s) ->
-        "Error line " ^ (string_of_int n) ^ " : " ^ s ^ "\n"
+        "Error syntax line " ^ (string_of_int n) ^ " : " ^ s ^ "\n"
 ;;
 
 let print_error_read (e: (int * string)) : unit = 
     print_string (get_string_error_read e)
+;;
+
+
+
+let get_string_error_eval (e: (int * string)) : string =
+    match e with
+    | (n, s) ->
+        "Error execution on line " ^ (string_of_int n) ^ " : " ^ s ^ "\n"
+;;
+
+let print_error_eval (e: (int * string)) : unit = 
+    print_string (get_string_error_eval e)
 ;;
